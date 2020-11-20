4e1760de2f91cc1e21e6f1674475c233c3cc4c4e
jaivalbhuptani <jaivalbhuptani1406@gmail.com>
2020-11-20 21:59:04 +0530

    secretSafe

--- ProjectHunt/settings.py
+++ ProjectHunt/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import secrets
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ziek1my0-fa6mgsy1ej@y2gk9tev8kf0o14lyu%0hcsh*yzh^8'
+SECRET_KEY = secrets.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
